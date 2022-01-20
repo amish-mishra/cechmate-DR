4ba934cdeca15f01154358a0a7bef1b6dbfb7611
Amish <87725042+amish-mishra@users.noreply.github.com>
2022-01-20 10:28:01 -0500

    changed name back to cechmate

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ else:
 with open('README.md') as f:
     long_description = f.read()
 
-setup(name='cechmate_DR',
+setup(name='cechmate',
       version=verstr,
       description='Custom filtration constructors for Python',
       long_description=long_description,
